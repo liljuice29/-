1f8f239c9b23394f043392f9c40badcc2f085a0e
huangwei <juice@qq.com>
2024-07-25 19:17:51 +0800

    全局文件添加

---
+++ 机房预约系统/globalfile.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//管理员文件
+#define admin_file "admin.txt"
+//学生文件
+#define student_file "student.txt"
+//教师文件
+#define teacher_file "teacher.txt"
+//机房信息文件
+#define computer_filw "computerroom.txt"
+//订单文件
+#define drder_file "order.txt"
